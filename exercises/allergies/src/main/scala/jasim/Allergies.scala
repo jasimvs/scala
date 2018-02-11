0b426858ba3a6cb9da6c153ebe9d694276484f1a
jasimvs <jas.imvs@gmail.com>
2018-02-11 13:14:49 +0700

    Allergies, first draft

---
+++ exercises/allergies/src/main/scala/jasim/Allergies.scala
@@ -0,0 +1,35 @@
+package jasim
+
+object Allergies {
+
+  def allergicTo(allergen: Allergen, score: Int): Boolean =
+    (score & allergen.code) == allergen.code
+
+  def list(score: Int): List[Allergen] = {
+    println("list")
+      Allergen.allergens.filter(allegen => allergicTo(allegen, score))
+    }
+}
+
+
+case class Allergen(code: Int) {
+  require(isPowerOf2(code))
+
+  def isPowerOf2(number: Int): Boolean = {
+    (code & (code - 1)) == 0
+  }
+}
+
+object Allergen {
+
+  val allergens = List(Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats)
+
+  val Eggs = Allergen(1)
+  val Peanuts = Allergen(2)
+  val Shellfish = Allergen(4)
+  val Strawberries = Allergen(8)
+  val Tomatoes = Allergen(16)
+  val Chocolate = Allergen(32)
+  val Pollen = Allergen(64)
+  val Cats = Allergen(128)
+}
